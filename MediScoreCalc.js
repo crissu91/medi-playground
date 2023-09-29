ee6fc41e1480a7ba4845ce7e213bc230060fd37a
crissu91 <cristinaf.stefan@gmail.com>
2023-09-29 14:24:47 +0100

    update the function with alerts for trends in medi score plus tests

--- MediScoreCalc.js
+++ MediScoreCalc.js
@@ -1,12 +1,10 @@
 const calculateRespirationScore = (respirationRange) => {
   switch (true) {
-    case respirationRange <= 8:
-      return 3;
     case respirationRange >= 9 && respirationRange <= 11:
       return 1;
     case respirationRange >= 21 && respirationRange <= 24:
       return 2;
-    case respirationRange >= 25:
+    case respirationRange <= 8 || respirationRange >= 25:
       return 3;
     default:
       return 0;
@@ -86,4 +84,32 @@ function mediScoreCalc(input) {
   return score;
 }
 
-module.exports = mediScoreCalc;
+function isWithin24Hours(date2, date1) {
+  let diff = (date2.getTime() - date1.getTime()) / 1000;
+  diff /= 60 * 60;
+  return Math.abs(Math.round(diff)) < 24;
+}
+function scoreDiff(score2, score1) {
+  if (score2 - score1 >= 2) {
+    return true;
+  }
+  return false;
+}
+
+function alert(patientData) {
+  const arrayOfScores = patientData.map((record) => ({
+    timeWhenTested: record.timeWhenTested,
+    score: mediScoreCalc(record.observations),
+  }));
+  const over24 = isWithin24Hours(
+    arrayOfScores[1].timeWhenTested,
+    arrayOfScores[0].timeWhenTested
+  );
+  const score = scoreDiff(arrayOfScores[1].score, arrayOfScores[0].score);
+
+  if (over24 && score) {
+    return "Additional risk: Score increased by more than 2 points within 24 hours";
+  } else return "No major changes";
+}
+
+module.exports = { mediScoreCalc, isWithin24Hours, scoreDiff, alert };
--- __tests__/medi-score.test.js
+++ __tests__/medi-score.test.js
@@ -1,4 +1,9 @@
-const mediScoreCalc = require("../MediScoreCalc");
+const {
+  mediScoreCalc,
+  isWithin24Hours,
+  scoreDiff,
+  alert,
+} = require("../MediScoreCalc");
 
 const patientList = [
   {
@@ -48,6 +53,51 @@ const patientList = [
     temperature: 38.5,
     CBG: 6,
     fasted: true,
+  },
+];
+
+const patientTestsOverTime1 = [
+  {
+    timeWhenTested: new Date("October 1, 2023 08:11:00"),
+    observations: {
+      airOrOxigen: "air",
+      consciousness: "alert",
+      respirationRange: 15,
+      spO2: 95,
+      temperature: 37.1,
+    },
+  },
+  {
+    timeWhenTested: new Date("October 1, 2023 10:25:00"),
+    observations: {
+      airOrOxigen: "oxigen",
+      consciousness: "CVPU",
+      respirationRange: 23,
+      spO2: 88,
+      temperature: 38.5,
+    },
+  },
+];
+const patientTestsOverTime2 = [
+  {
+    timeWhenTested: new Date("October 1, 2023 08:11:00"),
+    observations: {
+      airOrOxigen: "air",
+      consciousness: "alert",
+      respirationRange: 15,
+      spO2: 95,
+      temperature: 37.1,
+    },
+  },
+  {
+    timeWhenTested: new Date("October 1, 2023 10:25:00"),
+    observations: {
+      airOrOxigen: "air",
+      consciousness: "alert",
+      respirationRange: 15,
+      spO2: 95,
+      temperature: 37,
+    },
   },
 ];
 
@@ -145,5 +195,31 @@ describe("mediScoreCalc()", () => {
     expect(mediScoreCalc(patientList[3])).toBe(3);
     expect(mediScoreCalc(patientList[4])).toBe(4);
     expect(mediScoreCalc(patientList[5])).toBe(11);
+  });
+});
+describe("isWithin24Hours", () => {
+  test("should return true when the time difference is within 24 hours and false if not", () => {
+    const olderTimestamp1 = new Date("September 29, 2023 08:11:00");
+    const newerTimestamp2 = new Date("September 29, 2023 20:00:00");
+    expect(isWithin24Hours(newerTimestamp2, olderTimestamp1)).toBe(true);
+    const olderTimestamp3 = new Date("September 29, 2023 08:11:00");
+    const newerTimestamp4 = new Date("September 31, 2023 13:04:00");
+    expect(isWithin24Hours(newerTimestamp4, olderTimestamp3)).toBe(false);
+  });
+});
+describe("scoreDiff", () => {
+  test("should return true when the score difference is greater than or equal to 2 and false when is not", () => {
+    expect(scoreDiff(10, 8)).toBe(true);
+    expect(scoreDiff(10, 9)).toBe(false);
+  });
+});
+describe("alert", () => {
+  test("should return a risk alert if time difference is less than 24 hrs and score more than 2", () => {
+    expect(alert(patientTestsOverTime1)).toEqual(
+      "Additional risk: Score increased by more than 2 points within 24 hours"
+    );
+  });
+  test("should return an info alert if time difference is less than 24 hrs and score less than 2", () => {
+    expect(alert(patientTestsOverTime2)).toEqual("No major changes");
   });
 });
