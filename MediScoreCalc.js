f572c53943ed890704e473026faa70d32915fcfa
crissu91 <cristinaf.stefan@gmail.com>
2023-09-29 10:15:25 +0100

    update function to check respiration range and add tests

--- MediScoreCalc.js
+++ MediScoreCalc.js
@@ -14,7 +14,20 @@ function mediScoreCalc(input) {
   if (input.consciousness === "CVPU") {
     score += 3;
   }
-
+  switch (true) {
+    case input.respirationRange <= 8:
+      score += 3;
+      break;
+    case input.respirationRange >= 9 && input.respirationRange <= 11:
+      score += 1;
+      break;
+    case input.respirationRange >= 21 && input.respirationRange <= 24:
+      score += 2;
+      break;
+    case input.respirationRange >= 25:
+      score += 3;
+      break;
+  }
   return score;
 }
 
--- __tests__/medi-score.test.js
+++ __tests__/medi-score.test.js
@@ -14,7 +14,24 @@ describe("mediScoreCalc()", () => {
     expect(mediScoreCalc({ airOrOxigen: "oxigen" })).toBe(2);
   });
   test("should return a score when receiving consciousness as an input", () => {
-      expect(mediScoreCalc({ consciousness: "alert" })).toBe(0);
-      expect(mediScoreCalc({ consciousness: "CVPU" })).toBe(3);
+    expect(mediScoreCalc({ consciousness: "alert" })).toBe(0);
+    expect(mediScoreCalc({ consciousness: "CVPU" })).toBe(3);
+  });
+  test("should return a score when receiving respirationRange as an input", () => {
+    // less or equal to 8 should return 3
+    expect(mediScoreCalc({ respirationRange: 3 })).toBe(3);
+    expect(mediScoreCalc({ respirationRange: 8 })).toBe(3);
+    //  between 9 - 11 should return 1
+    expect(mediScoreCalc({ respirationRange: 9 })).toBe(1);
+    expect(mediScoreCalc({ respirationRange: 11 })).toBe(1);
+    // between 12 - 20 should return 0
+    expect(mediScoreCalc({ respirationRange: 12 })).toBe(0);
+    expect(mediScoreCalc({ respirationRange: 20 })).toBe(0);
+    // between 21 – 24 should return 2
+    expect(mediScoreCalc({ respirationRange: 21 })).toBe(2);
+    expect(mediScoreCalc({ respirationRange: 24 })).toBe(2);
+    // more or equal to 25 should return 3
+    expect(mediScoreCalc({ respirationRange: 25 })).toBe(3);
+    expect(mediScoreCalc({ respirationRange: 30 })).toBe(3);
   });
 });
